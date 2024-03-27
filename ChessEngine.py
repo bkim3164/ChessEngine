88162b49be53586854aa22c3e36b283791d961ff
Brian Kim <bkim3164@gmail.com>
2024-03-26 22:29:59 -0700

    Worked on basic UCI client

--- ChessEngine.py
+++ ChessEngine.py
@@ -5,49 +5,72 @@ import chess as ch
 class Engine:
     def __init__(self, board, depth):
         self.board = board
+        # self.FEN = self.board.board_fen()
+        #chess.Board, not chess.BaseBoard
         self.depth = depth
         self.CandidateMove = None
     
     #AlphaBeta Algorithmn
         
-    def alphaBetaMax(self, alpha : float, beta : float , depth : int):
+    # def alphaBetaMax(self, alpha : float, beta : float , depth : int):
 
-        #Base Case
-        if depth == 0 or self.board.legal_moves.count() == 0:
-            self.CandidateMove = move
-            return self.evaluate()
-        for move in self.board.legal_moves():
-            self.board.push(move)
-            #Temp alpha
-            temp_alpha = self.alphaBetaMin(alpha, beta, depth-1)
-            self.board.pop()
-            if temp_alpha >= beta:
-                self.CandidateMove = move
-                return beta
-            if temp_alpha > alpha:
-                alpha = temp_alpha
-        self.CandidateMove = move
-        return alpha
+    #     #Base Case
+    #     if depth == 0 or self.board2.legal_moves.count() == 0:
+    #         return self.evaluate()
+    #     for move in self.board2.legal_moves:
+    #         self.board2.push(move)
+    #         print(self.board2.fen())
+    #         temp_alpha = self.alphaBetaMin(alpha, beta, depth-1)
+    #         if temp_alpha >= beta:
+    #             return beta
+    #         if temp_alpha > alpha:
+    #             alpha = temp_alpha
+    #         self.board2.pop()
+    #     return alpha
         
     
-    def alphaBetaMin(self, alpha : float, beta : float, depth : int):
-        # Base Case
+    # def alphaBetaMin(self, alpha : float, beta : float, depth : int):
+    #     # Base Case
+    #     if depth == 0 or self.board2.legal_moves.count() == 0:
+    #         return self.evaluate()
+    #     for move in self.board2.legal_moves:
+    #         self.board2.push(move)
+    #         temp_beta = self.alphaBetaMin(alpha, beta, depth-1)
+    #         if temp_beta <= alpha:
+    #             return alpha
+    #         if temp_beta < beta:
+    #             beta = temp_beta
+    #         self.board2.pop()
+    #     return beta
+        
+
+    def minimax(self, depth, maxPlayer):
         if depth == 0 or self.board.legal_moves.count() == 0:
-            self.CandidateMove = move
             return self.evaluate()
-        for move in self.board.legal_moves():
-            self.board.push(move)
-            temp_beta = self.alphaBetaMin(alpha, beta, depth-1)
-            if temp_beta <= alpha:
-                self.CandidateMove = move
-                return alpha
-            if temp_beta < beta:
-                beta = temp_beta
-        self.CandidateMove = move
-        return beta
+        if maxPlayer:
+            maxEval = float('-inf')
+            # for move in self.board2.legal_moves:
+            #     print(f"LEGAL MOVE: {move} ", end = " ")
+            for move in self.board.legal_moves:
+                   self.board.push(move)
+                   eval = self.minimax(depth-1, False) 
+                   self.board.pop()
+                   maxEval = max(eval, maxEval)
+                   if(maxEval == eval):
+                        self.CandidateMove = move
+            return maxEval
+        else:
+            minEval = float('inf')
+            for move in self.board.legal_moves:
+                   self.board.push(move)
+                   eval = self.minimax(depth-1, True) 
+                   self.board.pop()
+                   minEval = min(eval, minEval)
+            return minEval            
+    
 
     
-    #Returns the evaluation function. If position is true, return white's pov
+    #Returns the evaluation function
     def evaluate(self) -> float:
         white_sum = 0
         black_sum = 0
@@ -58,8 +81,7 @@ class Engine:
                 black_sum += self.single_piece_eval(ch.SQUARES[i])
         return white_sum - black_sum
 
-
-
+    #Gets the evaluation of a single piece
     def single_piece_eval(self, square):
         if(self.board.piece_type_at(square) == ch.PAWN):
             return 1
@@ -73,3 +95,15 @@ class Engine:
             return 8    
         return 0 
     
+
+# UCI PROTOCOL 
+    
+while True:
+    args = input().split()
+    if args[:2] == ["position", "startpos"]:
+        pass
+    elif args[0] == "go":
+        cur_engine = Engine(ch.Board(ch.STARTING_BOARD_FEN), 1)
+        cur_engine.minimax(2, True)
+        move = cur_engine.CandidateMove
+        print("bestmove", move)
--- ChessEngineTests.py
+++ ChessEngineTests.py
@@ -5,14 +5,23 @@ import chess as ch
 class TestEvaluationFunction(unittest.TestCase):
 
     def test_start_board_eval_function(self):
-        test_engine = ce.Engine(ch.BaseBoard(ch.STARTING_BOARD_FEN), 3)
+        test_engine = ce.Engine(ch.Board(ch.STARTING_BOARD_FEN), 3)
         self.assertEqual(test_engine.evaluate(), 0)
     def test_one_white_pawn_eval(self):
-        test_engine = ce.Engine(ch.BaseBoard('8/8/8/8/8/3P4/8/8'), 3)
+        test_engine = ce.Engine(ch.Board('8/8/8/8/8/3P4/8/8'), 3)
         self.assertEqual(test_engine.evaluate(), 1)
     def test_one_white_pawn_eval_with_rook(self):
-        test_engine = ce.Engine(ch.BaseBoard('8/3r4/8/8/8/3P4/8/8'), 3)
+        test_engine = ce.Engine(ch.Board('8/3r4/8/8/8/3P4/8/8'), 3)
         self.assertEqual(test_engine.evaluate(), -4)
+
+class TestAlphaBetaPruning(unittest.TestCase):
+
+    def test_start_board_eval_function(self):
+        test_engine = ce.Engine(ch.Board('1k6/2p1p3/8/8/8/8/4R3/1K6'), 2)
+        score = test_engine.minimax(1, True)
+        self.assertEqual(score, 4)
+        print(test_engine.CandidateMove)
+
 
 
 
